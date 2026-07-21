7d4295338e7de8b165666f1a35c28b5631ef3137
bistpramod <bistpramod113@gmail.com>
2026-07-21 21:47:03 +0545

    update

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,25 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
-import authRoutes from "./routes/auth.routes.js"
-import productRoutes from './routes/product.routes.js'
-import cartRoutes from './routes/cart.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
 import addressRoutes from "./routes/address.routes.js";
+import orderRoutes from "./routes/order.routes.js";
 
 dotenv.config();
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoutes)
-app.use('/api/products',productRoutes)
-app.use('/api/cart', cartRoutes)
-app.use('/api/address',addressRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
 connectDB();
 
 app.listen(5002, () => {
-    console.log("server is running");
+  console.log("server is running");
 });
